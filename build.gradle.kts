04087c3016a2fb96130f73cc42dd7e95423b8ded
人間工作 <coolxiaom95@gmail.com>
2025-03-21 22:07:31 +0800

    use kotlin dsl build script

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    java
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "me.lagbug"
+version = "2.1.8"
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    maven("https://repo.rosewooddev.io/repository/public/")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot:1.16.5")
+    compileOnly("me.clip:placeholderapi:2.11.6")
+
+    implementation("com.sun.mail:javax.mail:1.6.2")
+    implementation("javax.activation:activation:1.1.1")
+}
+
+val targetJavaVersion = 8
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+    }
+}
+tasks {
+    shadowJar {
+        dependencies {
+            include(dependency("com.sun.mail:javax.mail:1.6.2"))
+            include(dependency("javax.activation:activation:1.1.1"))
+        }
+        archiveClassifier.set("")
+    }
+    withType<JavaCompile> {
+        if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+            options.release.set(targetJavaVersion)
+        }
+    }
+    processResources {
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        from(sourceSets.main.get().resources.srcDirs) {
+            expand("version" to version)
+            include("plugin.yml")
+        }
+    }
+}
